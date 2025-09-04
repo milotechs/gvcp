7ed2dc48bd29a59ca65327a85ea58322690bfd8c
DENFAS SIMFUKWE <denfassimfukwe202@gmail.com>
2025-09-04 19:25:01 +0200

    Update home_css.php

--- Scripts/home_css.php
+++ Scripts/home_css.php
@@ -26,4 +26,94 @@
      font-size: 7mm;
      font-weight: 200;
    }
+<<<<<<< Updated upstream
+=======
+
+   /* --------- why choose us/ core values---------*/
+ 
+   #core-values
+   {
+      margin-top: 6cm; 
+   }
+
+   #why-choose-us img, 
+   #core-values img
+   {
+      width: 50px;
+      height : auto;
+      margin-bottom : 3mm;
+   }
+
+   #why-choose-us .col-md-4,
+   #core-values .col-md-4
+   {
+      padding-bottom : 10mm;
+   }
+
+   #why-choose-us .row,
+   #core-values .row
+   {
+      padding-top: 5mm;
+   }
+
+/* --------- services---------*/
+  #services
+  {
+    background-image: url('/gvcp/bg/services.jpg');
+    background-position: center;
+    background-size: cover;
+    color : white;
+    background-attachment: fixed;
+  }
+
+  #services button
+  {
+    border: none;
+    background-color : orange;
+    font-size : 4mm;
+    padding : 2mm 6mm;
+    border-radius: 1mm;
+  }
+
+  #services .col-md-6
+  {
+    padding-bottom : 10mm;
+  }
+
+  #services p
+  {
+    font-weight: 200;
+  }
+
+  #services-dark-bg
+  {
+    background-color: rgb(0,0,0,0.5);
+    padding : 3cm 0mm;
+  }
+
+   #services hr
+   {
+     background-color: orange;
+   }
+
+   
+   #services img
+   {
+     width : 100%;
+     height : 200px;
+     object-fit : cover;
+     border-radius: 2mm;
+     margin-bottom : 3mm
+   }
+
+   /* --------- why choose us ---------*/
+    #why-choose-img img
+    {
+        width: 100%;
+        height: 700px;
+        object-fit: cover;
+        border-radius: 2mm; 
+    }
+    
+>>>>>>> Stashed changes
 </style>
