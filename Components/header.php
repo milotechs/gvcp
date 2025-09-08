8e71e8832c89255836a8e915b65ba17deb7d15cb
DENFAS SIMFUKWE <denfassimfukwe202@gmail.com>
2025-09-08 10:25:52 +0200

    denfas

--- Components/header.php
+++ Components/header.php
@@ -4,18 +4,16 @@
             <img src = '<?php echo $BASE_URL;?>/Icons/logo.png'>
         </div>
         <div id = 'navi' align = 'right'>
-            <a href = '#about' align = 'left'>
+            <a href = '<?php echo $BASE_URL;?>/home' align = 'left'>
+                Home
+            </a>
+            <a href = '<?php echo $BASE_URL;?>/about' align = 'left'>
                 About
             </a>
-            <a href = '#about' align = 'left'>
+            <a href = '<?php echo $BASE_URL;?>/services' align = 'left'>
                 Services
             </a>
-            <a  href = '#mission' align = 'left'>
-                Mission
-            </a>
-            <a href = '#vision' align = 'left'>
-                Vision
-            </a>
+            
             <a href = '#contacts' align = 'left'>
                 Contacts
             </a>
---
+++ View/services.php
@@ -0,0 +1,29 @@
+<div id = 'home'>
+    <div id = 'hero'>
+        <div class = 'container'>
+            <h1>Construction</h1>
+        </div>
+    </div> 
+    <div class = 'container'>
+        <div class = 'row'>
+            <div class = 'col-md-6'>
+                    <div id =  'title'>
+                        <h1 class="section-title">Our Services</h1>
+                        
+                        <p>
+                            
+                        </p>
+                        <div id = 'header-socials'>
+                            <a href = '#about' align = 'left'>
+                              <img src = '<?php echo $BASE_URL;?>/Icons/fb-black.png'>
+                            </a>
+                            <a>
+                              <img src = '<?php echo $BASE_URL;?>/Icons/in-black.png'>
+                            </a>
+                            <a>
+                              <img src = '<?php echo $BASE_URL;?>/Icons/gram-black.png'>
+                            </a>
+                       </div>
+                    </div>
+            </div>  
+       </div>  
